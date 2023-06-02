c5aaabc08a63ceebad60900369c4e6702930c499
Bryce Bryce <bpearce@angelo.edu>
2023-06-01 22:28:50 -0700

    left/right movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,21 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(dirX * 7f, rigidBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 14f);
         }
     }
 }
